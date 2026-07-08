ff42da982a1ce3b81571f1e5e95732fba42db8db
zznty <94796179+zznty@users.noreply.github.com>
2026-07-08 22:33:26 +0700

    fix: use pipeline-based null coercion for releaseTime in forge.ps1

--- servers-meta-builder/forge.ps1
+++ servers-meta-builder/forge.ps1
@@ -38,7 +38,7 @@ foreach ($badVersion in ($forgeVersions.versions.Keys | Where-Object {
     uid           = $uid;
     versions      = $forgeVersions.versions.Values | ForEach-Object {
         [PSCustomObject]@{
-            releaseTime = EmptyToNull (if (Test-Path "$PSScriptRoot/meta-upstream/forge/version_manifests/$($_.longversion).json") { (Get-Content "$PSScriptRoot/meta-upstream/forge/version_manifests/$($_.longversion).json" | ConvertFrom-Json | Select-Object -ExpandProperty releaseTime) } else { "1970-01-01T00:00:00+00:00" });
+            releaseTime = (if (Test-Path "$PSScriptRoot/meta-upstream/forge/version_manifests/$($_.longversion).json") { (Get-Content "$PSScriptRoot/meta-upstream/forge/version_manifests/$($_.longversion).json" | ConvertFrom-Json | Select-Object -ExpandProperty releaseTime) } else { "1970-01-01T00:00:00+00:00" }) | ForEach-Object { if ($_) { $_ } else { $null } };
             version     = $_.version
             recommended = $_.recommended
             sha1        = Get-FileHash "$PSScriptRoot/meta-upstream/forge/files_manifests/$($_.longversion).json" -Algorithm SHA1 | Select-Object -ExpandProperty Hash
